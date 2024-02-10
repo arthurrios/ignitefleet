f9bae98b309cb1fa78a05eaba41fc5d043ee3ca0
arthur <arthur.rios007@gmail.com>
2024-02-10 07:43:00 -0300

    feat: add google sign in

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,34 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@dtos': './src/dtos',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@contexts': './src/contexts',
+            '@routes': './src/routes',
+            '@libs': './src/libs',
+          },
+        },
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          allowUndefined: false,
+        },
+      ],
+    ],
+  }
+}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,49 @@
+import { useState } from 'react'
+import { Alert } from 'react-native'
 import { Container, Slogan, Title } from './styles'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
 
 import backgroundImg from '../../assets/background.png'
 import { Button } from '../../components/Button'
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from '@env'
+
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+})
 
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true)
+
+      const { idToken } = await GoogleSignin.signIn()
+
+      if (idToken) {
+      } else {
+        Alert.alert('Sign in', 'Not possible to log in to your google account.')
+        setIsAuthenticating(false)
+      }
+    } catch (error) {
+      console.log(error)
+      setIsAuthenticating(false)
+      Alert.alert('Sign in', 'Not possible to log in to your google account.')
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Vehicle Use Management</Slogan>
 
-      <Button title="Sign in with Google" />
+      <Button
+        title="Sign in with Google"
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
+      />
     </Container>
   )
 }
