abe50a3c9fcfa3ae15bbaa448e1bb31c02f78555
arthur <arthur.rios007@gmail.com>
2024-02-10 17:07:00 -0300

    feat: add navigation to departure

--- src/components/CarStatus/index.tsx
+++ src/components/CarStatus/index.tsx
@@ -1,12 +1,13 @@
 import { Car, Key } from 'phosphor-react-native'
 import { Container, IconBox, Message, TextHighlight } from './styles'
 import { useTheme } from 'styled-components/native'
+import { TouchableOpacityProps } from 'react-native'
 
-type Props = {
+type Props = TouchableOpacityProps & {
   licencePlate?: string | null
 }
 
-export function CarStatus({ licencePlate = null }: Props) {
+export function CarStatus({ licencePlate = null, ...props }: Props) {
   const theme = useTheme()
 
   const Icon = licencePlate ? Key : Car
@@ -16,7 +17,7 @@ export function CarStatus({ licencePlate = null }: Props) {
   const status = licencePlate ? 'arrival' : 'departure'
 
   return (
-    <Container>
+    <Container {...props}>
       <IconBox>
         <Icon size={32} color={theme.COLORS.BRAND_LIGHT} />
       </IconBox>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,21 @@
 import { HomeHeader } from '@components/HomeHeader'
 import { Container, Content } from './styles'
 import { CarStatus } from '@components/CarStatus'
+import { useNavigation } from '@react-navigation/native'
 
 export function Home() {
+  const { navigate } = useNavigation()
+
+  function handleRegisterMovement() {
+    navigate('departure')
+  }
+
   return (
     <Container>
       <HomeHeader />
 
       <Content>
-        <CarStatus licencePlate="XXX-0000" />
+        <CarStatus onPress={handleRegisterMovement} />
       </Content>
     </Container>
   )
