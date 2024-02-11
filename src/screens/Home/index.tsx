d5c456c0b3750e4921fa2346ea9250983b878f8b
arthur <arthur.rios007@gmail.com>
2024-02-11 10:40:10 -0300

    feat: fetch vehicle data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,30 @@
-import { HomeHeader } from '@components/HomeHeader'
-import { Container, Content } from './styles'
-import { CarStatus } from '@components/CarStatus'
 import { useNavigation } from '@react-navigation/native'
+
+import { useQuery } from '@libs/realm'
+
+import { HomeHeader } from '@components/HomeHeader'
+import { CarStatus } from '@components/CarStatus'
+
+import { Container, Content } from './styles'
+import { History } from '@libs/realm/schemas/History'
+import { useEffect } from 'react'
 
 export function Home() {
   const { navigate } = useNavigation()
 
+  const history = useQuery(History)
+
   function handleRegisterMovement() {
     navigate('departure')
   }
+
+  function fetchVehicle() {
+    console.log(history)
+  }
+
+  useEffect(() => {
+    fetchVehicle()
+  }, [])
 
   return (
     <Container>
