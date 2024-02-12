8ca223cb8171d8f1c9f11b88c7a758b7d4ccb6d1
arthur <arthur.rios007@gmail.com>
2024-02-12 18:05:32 -0300

    storage: add storage functions

---
+++ src/libs/storage/locationStorage.ts
@@ -0,0 +1,29 @@
+import { MMKV } from 'react-native-mmkv'
+
+export const storage = new MMKV()
+
+const STORAGE_KEY = '@ignitefleet:location'
+
+type LocationProps = {
+  latitude: number
+  longitude: number
+  timestamp: number
+}
+
+export function getStorageLocations() {
+  const storagedLocations = storage.getString(STORAGE_KEY)
+  const response = storage ? JSON.parse(String(storagedLocations)) : []
+
+  return response
+}
+
+export function saveStorageLocation(newLocation: LocationProps) {
+  const storagedLocations = getStorageLocations()
+  storagedLocations.push(newLocation)
+
+  storage.set(STORAGE_KEY, JSON.stringify(storagedLocations))
+}
+
+export function removeStorageLocations() {
+  storage.delete(STORAGE_KEY)
+}
